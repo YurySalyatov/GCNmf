333af80bbcf27a3d964f69017e573b703835fb4b
yurys <yury.salyatov@gmail.com>
2025-05-09 11:34:38 +0300

    add experiment2

--- run_node_cls.py
+++ run_node_cls.py
@@ -11,6 +11,7 @@ from utils import NodeClsData, apply_mask, generate_mask
 import matplotlib.pyplot as plt
 import numpy as np
 from tabulate import tabulate
+import os
 
 feature_mean = {'citeseer': 0.0002688338, 'cora': 0.0023735422}
 feature_std = {'citeseer': 0.0006978367, 'cora': 0.0056594303}
@@ -38,171 +39,169 @@ args = parser.parse_args()
 dataset_str = args.dataset
 noise_levels = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-if __name__ == '__main__':
-    def add_noisy_features(data, noise_level, convert=True):
-        noisy_data = data.clone()
+
+
+def save_table(results, filename="results_experiment1_table.txt"):
+    all_keys = [k for k in results[0].keys() if k != 'sigma']
+    # Создаем список форматов: первый столбец .2f, остальные .5f
+    headers = ["Noise Level"] + all_keys
+    float_fmts = [".2f"] + [".5f"] * (len(headers) - 1)
+    data = [[res[key] for res in results] for key in results[0].keys()]
+    rows = list(zip(*data))
+    # Генерируем таблицу
+    table_str = tabulate(
+        rows,
+        headers=headers,
+        tablefmt="grid",
+        floatfmt=float_fmts
+    )
+
+    # Сохраняем в файл
+    with open(filename, "w") as f:
+        f.write(table_str)
+
+    return table_str
+
+
+def node_noise(data, percentage, convert=True):
+    """
+    Заменяет все фичи у случайно выбранного процента вершин на значения из общего распределения тензора
+    Args:
+        tensor: исходный тензор (num_nodes, num_features)
+        percentage: процент вершин для замены (0.0 - 1.0)
+    Returns:
+        тензор с шумом
+    """
+    tensor = data.features
+    if percentage <= 0:
+        res = data.clone()
         if convert:
-            noisy_data.to(device)
-        a, b = data.features.shape
+            res.to(device)
+        return res
+
+    num_nodes = tensor.size(0)
+    num_selected = int(percentage * num_nodes)
+
+    if num_selected == 0:
+        res = data.clone()
         if convert:
-            # Конвертируем константы в тензоры на нужном устройстве
-            feature_mean_tensor = torch.tensor(feature_mean[dataset_str],
-                                               dtype=torch.float32,
-                                               device=device)  # (2)
-            feature_std_tensor = torch.tensor(feature_std[dataset_str],
-                                              dtype=torch.float32,
-                                              device=device)  # (2)
-        else:
-            feature_mean_tensor = torch.tensor(feature_mean[dataset_str],
-                                               dtype=torch.float32)  # (2)
-            feature_std_tensor = torch.tensor(feature_std[dataset_str],
-                                              dtype=torch.float32)  # (2)
+            res.to(device)
+        return res
 
-        # Создаем шум НА GPU (если device='cuda')
-        if convert:
-            noise = torch.randn(a, b, device=device)  # (3)
-        else:
-            noise = torch.randn(a, b)
+    # Выбираем случайные вершины
+    selected_nodes = torch.randperm(num_nodes)[:num_selected]
 
-        # Вычисляем шумовую компоненту
-        noise_component = (feature_mean_tensor + 2 * noise * torch.sqrt(feature_std_tensor)) * noise_level * 0.1
+    # Генерируем значения для замены из общего распределения
+    flattened = tensor.flatten()
+    shuffled_values = flattened[torch.randperm(len(flattened))][:num_selected * tensor.size(1)]
+    replacement = shuffled_values.view(num_selected, tensor.size(1))
 
-        # Применяем к features (уже на GPU)
-        noisy_data.features += noise_component
-
-        return noisy_data
-
-
-    for main_sigma in noise_levels:
-        data = NodeClsData(args.dataset)
-        data = add_noisy_features(data, main_sigma, False)
-        mask = generate_mask(data.features, args.rate, args.type)
-        apply_mask(data.features, mask)
-        model = GCNmf(data, nhid=args.nhid, dropout=args.dropout, n_components=args.ncomp)
-        params = {
-            'lr': args.lr,
-            'weight_decay': args.wd,
-            'epochs': args.epoch,
-            'patience': args.patience,
-            'early_stopping': True
-        }
-        trainer = NodeClsTrainer(data, model, params, niter=10, verbose=args.verbose)
-        trainer.run()
-        model.load_state_dict(torch.load("trained_model/without_noisy.pkl"))
-        fix_model = model
-        # fix_model.to(device)
-        noisy_data = data.clone()
+    # Создаем копию и применяем шум
+    noised_tensor = tensor.clone()
+    noised_tensor[selected_nodes] = replacement
+    noisy_data = data.clone()
+    noisy_data.features = noised_tensor
+    if convert:
         noisy_data.to(device)
+    return noisy_data
 
 
-        def compute_entropy(log_probs):
-            return -torch.sum(torch.exp(log_probs) * log_probs, dim=1)
+def feature_noise(data, percentage, convert=True):
+    """
+    Заменяет случайный процент фичей для всех вершин на значения из общего распределения тензора
+    Args:
+        tensor: исходный тензор (num_nodes, num_features)
+        percentage: процент фичей для замены (0.0 - 1.0)
+    Returns:
+        тензор с шумом
+    """
+    tensor = data.features
+    if percentage <= 0:
+        res = data.clone()
+        if convert:
+            res.to(device)
+        return res
+
+    num_features = tensor.size(1)
+    num_selected_features = int(percentage * num_features)
+
+    if num_selected_features == 0:
+        res = data.clone()
+        if convert:
+            res.to(device)
+        return res
+
+    # Выбираем случайные фичи
+    selected_features = torch.randperm(num_features)[:num_selected_features]
+
+    # Генерируем значения для замены
+    flattened = tensor.flatten()
+    shuffled_values = flattened[torch.randperm(len(flattened))][:tensor.size(0) * num_selected_features]
+    replacement = shuffled_values.view(tensor.size(0), num_selected_features)
+
+    # Создаем копию и применяем шум
+    noised_tensor = tensor.clone()
+    noised_tensor[:, selected_features] = replacement
+    noisy_data = data.clone()
+    noisy_data.features = noised_tensor
+    if convert:
+        noisy_data.to(device)
+    return noisy_data
 
 
-        def compute_margin(log_probs):
-            exp = torch.exp(log_probs)
-            exp, _ = torch.sort(exp, dim=1, descending=True)
-            return exp[:, 0] - exp[:, 1]
+if __name__ == '__main__':
+    methods = [feature_noise, node_noise]
+    for dataset_name in ['cora', 'citeseer']:
+        data = NodeClsData(dataset_name)
+        for method in methods:
+            tables_dir = f"results/{dataset_name}/{method.__name__}"
+            os.makedirs(tables_dir, exist_ok=True)
+            results = []
+            for sigma in noise_levels:
+                noisy_data = method(data, sigma, False)
+                mask = generate_mask(noisy_data.features, args.rate, args.type)
+                apply_mask(noisy_data.features, mask)
+                model = GCNmf(noisy_data, nhid=args.nhid, dropout=args.dropout, n_components=args.ncomp)
+                params = {
+                    'lr': args.lr,
+                    'weight_decay': args.wd,
+                    'epochs': args.epoch,
+                    'patience': args.patience,
+                    'early_stopping': True
+                }
+                trainer = NodeClsTrainer(noisy_data, model, params, niter=10, verbose=args.verbose)
+                trainer.run()
+                model.load_state_dict(torch.load("trained_model/without_noisy.pkl"))
+                model.to(device)
+                noisy_data.to(device)
 
 
-        results = []
-        pu_model = []
-        for sigma in noise_levels[1:]:
-            # Добавление шума
-            noisy_data = add_noisy_features(data, sigma)
-            # print("shape noisy data", noisy_data.x.shape)
-            # print(fix_model)
-            # Оценка PU
-            fix_model.eval()
-            fix_model.set_eval()
-            with torch.no_grad():
-                log_probs = fix_model(noisy_data)
-                # print("out", out.shape)
-                # print(probs.shape)
-                entropy = compute_entropy(log_probs[noisy_data.test_mask])
-                margin = compute_margin(log_probs[noisy_data.test_mask])
-                # print(entropy.shape)
-                e_pu = entropy.mean().item()
-                m_pu = margin.mean().item()
-
-            # Оценка AU (100 итераций с шумом)
-            num_samples = 100
-            predictions = []
-            perturbed_data = data.clone()
-            perturbed_data.to(device)
-            fix_model.train()
-            fix_model.set_train()
-            fix_model.set_dropout(sigma)
-            entropy = []
-            var_pred = []
-            for _ in range(num_samples):
-                with torch.no_grad():
-                    log_probs = fix_model(perturbed_data)[perturbed_data.test_mask]
-                    predictions.append(log_probs)
-                    entropy.append(compute_entropy(log_probs))
-
-            for _ in range(num_samples // 10):
-                perturbed_data = add_noisy_features(perturbed_data, sigma)
-                with torch.no_grad():
-                    log_probs = fix_model(perturbed_data)
-                    exp = torch.exp(log_probs)
-                    var_pred.append(exp)
-
-            predictions = torch.stack(predictions)
-            entropy = torch.stack(entropy)
-            # print(predictions.shape)
-            mean_pred = predictions.mean(dim=0)
-            mean_pred_entropy = compute_entropy(mean_pred)
-            mean_entropy_pred = entropy.mean(dim=0)
-            # au_arr = torch.var(predictions, dim=0)
-            au = mean_entropy_pred.mean().item()
-            # print(au_arr.shape)
-            # au = au_arr.mean().item()
-            var_pred = torch.stack(var_pred)
-            var_au_nodes_class = torch.var(var_pred, dim=0)
-            var_au_nodes = torch.sum(var_au_nodes_class, dim=1)
-            var_au = var_au_nodes.mean()
-            results.append({'sigma': sigma, 'PU': e_pu, 'M PU': m_pu, 'AU': au, 'Var AU': var_au})
-            print(
-                f"Noise: {sigma:.2f} | Entropy PU: {e_pu:.4f} | Margin PU: {m_pu:.4f} | AU: {au:.4f} | Var AU: {var_au:.4f}")
+                def compute_entropy(log_probs):
+                    return -torch.sum(torch.exp(log_probs) * log_probs, dim=1)
 
 
-        # Визуализация
-        def plt_res(name):
-            plt.figure(figsize=(10, 6))
-            plt.plot(
-                [sigma for sigma in noise_levels][1:],
-                [res[name] for res in results],
-                marker='o',
-                label=f'{name} (Entropy)'
-            )
-            plt.xlabel('Noise Level')
-            plt.ylabel('Uncertainty')
-            plt.legend()
-            plt.title('Dependence of P Uncertainties on Noise Level')
-            plt.grid(True)
-            plt.show()
+                # Оценка AU (100 итераций с шумом)
+                num_samples = 100
+                model.train()
+                model.set_train()
+                entropy = []
+                predictions = []
+                for _ in range(num_samples):
+                    with torch.no_grad():
+                        log_probs = model(noisy_data)
+                        predictions.append(torch.exp(log_probs))
+                        entropy.append(compute_entropy(log_probs))
 
-
-        # print(f"A Uncertainties:", au)
-        # us = ['PU', 'AU', 'M PU']
-        # for u in us:
-        #     plt_res(u)
-
-        data = list(zip(noise_levels[1:], [res['PU'] for res in results], [res['AU'] for res in results],
-                        [res["M PU"] for res in results], [res["Var AU"] for res in results]))
-        headers = ["Noisy Level", "PU", "AU", "M PU", "Var AU"]
-        table_str = tabulate(
-            data,
-            headers=headers,
-            tablefmt="grid",
-            floatfmt=".5f"
-        )
-
-        # Сохраняем в файл
-        with open(f"results_final_GCNmf_{dataset_str}_{main_sigma}.txt", "w", encoding="utf-8") as f:
-            f.write(table_str)
-        # print(tabulate(data,
-        #                headers=headers,
-        #                tablefmt="grid",  # можно изменить на "simple", "fancy_grid" и др.
-        #                floatfmt=".5f"))  # формат чисел
+                predictions = torch.stack(predictions)
+                entropy = torch.stack(entropy)
+                mean_pred = predictions.mean(dim=0)
+                mean_pred_entropy = -torch.sum(mean_pred * torch.log(mean_pred), dim=1)
+                mean_entropy_pred = entropy.mean(dim=0)
+                d_au = mean_entropy_pred.mean().item()
+                d_pu = mean_pred_entropy.mean().item()
+                mu = d_pu - d_au
+                results.append(
+                    {'sigma': sigma, "Dropout PU": d_pu, 'Dropout AU': d_au, "Dropout MU": mu})
+                print(f"noisy: {sigma}")
+                print(f"Dropout PU: {d_pu:.4f}, Dropout AU: {d_au:.4f}, Dropout MU: {mu:.4f}")
+                save_table(results, tables_dir + f"/GCNmf_experiment2.txt")
