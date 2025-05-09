4dbe50a1436865b933195fa68347dbc413747dfa
yurys <yury.salyatov@gmail.com>
2025-05-09 15:03:11 +0300

    add another load

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -15,7 +15,7 @@ import torch
 class Data:
     def __init__(self, dataset_str):
         if dataset_str in ['cora', 'citeseer']:
-            data = load_planetoid_data(dataset_str)
+            data = universal_load_data(dataset_str, return_dict=True)
         elif dataset_str in ['amaphoto', 'amacomp']:
             data = load_amazon_data(dataset_str)
         else:
@@ -118,6 +118,122 @@ class LinkPredData(Data):
         self.adjmat = self.adjmat.to(device)
 
 
+def sparse_matrix_to_torch(mat):
+    mat = mat.tocoo()
+    indices = torch.LongTensor(np.vstack((mat.row, mat.col)))
+    values = torch.FloatTensor(mat.data)
+    return torch.sparse.FloatTensor(indices, values, torch.Size(mat.shape))
+
+def universal_load_data(
+        dataset_str,
+        norm_adj=True,
+        add_self_loops_flag=True,
+        return_dict=False,
+        return_indices=True,
+        generative_flag=False
+):
+    """
+    Универсальная функция загрузки данных для GNN
+
+    Параметры:
+    dataset_str: str - название датасета (cora, citeseer, pubmed)
+    norm_adj: bool - нормализовать матрицу смежности (default: True)
+    add_self_loops_flag: bool - добавить self-loops (default: True)
+    return_dict: bool - возвращать словарь вместо кортежа (default: False)
+    return_indices: bool - возвращать train/val/test индексы (default: True)
+    generative_flag: bool - не нормализовать фичи (default: False)
+
+    Возвращает:
+    Зависит от флагов:
+    - По умолчанию: (adj, features, labels, idx_train, idx_val, idx_test)
+    - При return_dict: словарь с полным набором данных
+    """
+
+    # Загрузка raw данных
+    try:
+        names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+        objects = []
+        for name in names:
+            with open(f"./data/planetoid/ind.{dataset_str}.{name}", 'rb') as f:
+                if sys.version_info > (3, 0):
+                    objects.append(pkl.load(f, encoding='latin1'))
+                else:
+                    objects.append(pkl.load(f))
+        x, y, tx, ty, allx, ally, graph = objects
+    except:
+        raise ValueError(f"Cannot load dataset: {dataset_str}")
+
+    # Обработка индексов
+    test_idx_reorder = parse_index_file(f"./data/planetoid/ind.{dataset_str}.test.index")
+    test_idx_range = np.sort(test_idx_reorder)
+
+    if dataset_str == 'citeseer':
+        # Fix citeseer dataset (there are some isolated nodes in the graph)
+        # Find isolated nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder) + 1)
+        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        tx_extended[test_idx_range - min(test_idx_range), :] = tx
+        tx = tx_extended
+        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+        ty_extended[test_idx_range - min(test_idx_range), :] = ty
+        ty = ty_extended
+
+    # Объединение фичей
+    features = sp.vstack((allx, tx)).tolil()
+    features[test_idx_reorder, :] = features[test_idx_range, :]
+
+    # Обработка меток
+    labels = np.vstack((ally, ty))
+    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+    labels = torch.LongTensor(np.argmax(labels, axis=1))
+
+    features = torch.FloatTensor(np.array(features.todense()))
+
+    # Построение графа
+    G = nx.from_dict_of_lists(graph)
+    edge_list = adj_list_from_dict(graph)
+
+    # Матрица смежности
+    adj = nx.adjacency_matrix(G)
+    if add_self_loops_flag:
+        adj = adj + sp.eye(adj.shape[0])
+
+    # Нормализация adjacency
+    if norm_adj:
+        adj = normalize_adj2(adj)
+
+    # Конвертация в тензоры
+    adj = sparse_matrix_to_torch(adj)
+
+    # Подготовка индексов
+    if return_indices:
+        idx_test = test_idx_range.tolist()
+        idx_train = list(range(len(y)))
+        idx_val = list(range(len(y), len(y) + 500))
+        idx_train = torch.LongTensor(idx_train)
+        idx_val = torch.LongTensor(idx_val)
+        idx_test = torch.LongTensor(idx_test)
+    else:
+        idx_train, idx_val, idx_test = None, None, None
+
+    # Формирование результата
+    if return_dict:
+        return {
+            'adj': adj,
+            'features': features,
+            'labels': labels,
+            'edge_list': edge_list,
+            'G': G,
+            'idx_train': idx_train,
+            'idx_val': idx_val,
+            'idx_test': idx_test
+        }
+    else:
+        result = (adj, features, labels)
+        if return_indices:
+            result += (idx_train, idx_val, idx_test)
+        return result
+
 def load_planetoid_data(dataset_str):
     """
 
@@ -131,7 +247,7 @@ def load_planetoid_data(dataset_str):
     data: dict
 
     """
-    names = ['tx', 'ty', 'allx', 'ally', 'graph']
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
     objects = []
     for name in names:
         with open("data/planetoid/ind.{}.{}".format(dataset_str, name), 'rb') as f:
@@ -146,7 +262,7 @@ def load_planetoid_data(dataset_str):
                 out = out.todense() if hasattr(out, 'todense') else out
                 objects.append(torch.tensor(out))
 
-    tx, ty, allx, ally, graph = tuple(objects)
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
     test_idx = parse_index_file("data/planetoid/ind.{}.test.index".format(dataset_str))
     sorted_test_idx = np.sort(test_idx)
 
@@ -317,3 +433,10 @@ def normalize_adj(edge_list):
     v = deg_inv_sqrt[row] * weight * deg_inv_sqrt[col]
     norm_adj = torch.sparse.FloatTensor(edge_list, v)
     return norm_adj
+
+def normalize_adj2(adj):
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
